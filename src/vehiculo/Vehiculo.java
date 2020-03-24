4a8d2e3dada134674ab247033436b01702772c50
Danny_PC <Danny_PC@Equipo-0c10>
2020-03-23 22:27:08 -0600

    agregacion clase vehiculo

---
+++ src/vehiculo/Vehiculo.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehiculo;
+
+/**
+ *
+ * @author Danny_PC
+ */
+public class Vehiculo {
+    private String placa;
+    private String descripcion;
+
+    public Vehiculo() {
+        this.placa = null;
+        this.descripcion = null;
+    }
+
+    public Vehiculo(String placa, String descripcion) {
+        this.placa = placa;
+        this.descripcion = descripcion;
+    }
+
+    public Vehiculo(String placa) {
+        this.placa = placa;
+      
+    }
+    
+    public boolean Comprobar() {
+        //retorna true si no estan vacios
+        return  this.descripcion!=null & this.placa!=null;
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    //metodos de acceso
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
