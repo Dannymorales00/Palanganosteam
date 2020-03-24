db2103934fbe7fecd57b4f5c46fa2e8563c2498c
Danny_PC <Danny_PC@Equipo-0c10>
2020-03-24 12:45:59 -0600

    creando ventana para el archivo config

--- src/config/Manejoconfig.java
+++ src/config/Manejoconfig.java
@@ -4,12 +4,37 @@
  * and open the template in the editor.
  */
 package config;
-
+import java.io.File;
+import org.ini4j.*;
 /**
  *
  * @author Danny_PC
  */
 public class Manejoconfig {
+
+    public void leer(String[] args) {
+        
+    
+        try{
+            Wini ini = new Wini(new File("C:\\Users\\sdkca\\Desktop\\myinifile.ini"));
+            int age = ini.get("owner", "age", int.class);
+            double height = ini.get("owner", "height", double.class);
+            String server = ini.get("database", "server");
+            
+            
+            System.out.print("Edad: " + age + "\n");
+            System.out.print("Altura: " + height + "\n");
+            System.out.print("IP Servidor: " + server + "\n");
+            // Para atrapar cual excepción relacionada a encontrar el archivo p. ej
+            // (The system cannot find the file specified)
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+    
+    }
+    
+    
+    
     
     
     
