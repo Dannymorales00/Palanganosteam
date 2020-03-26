b2fbbd213c5c903c287f66624898f010ce4f4f03
Danny_PC <Danny_PC@Equipo-0c10>
2020-03-25 18:38:45 -0600

    controlador bitacora agregado

--- src/controlador/ControladorBitacora.java
+++ src/controlador/ControladorBitacora.java
@@ -5,15 +5,46 @@
  */
 package controlador;
 
+import conexion.Conexion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import modelo.Bitacora;
+
+
 /**
  *
  * @author Danny_PC
  */
+
+
+
+
 public class ControladorBitacora {
     
+    private Conexion conn;
+    private Statement sentencias = this.conn.getSentencias();
+    private ResultSet datos = this.conn.getDatos();
+
+    public ControladorBitacora(Conexion conn) {
+        this.conn = conn;
+    }
+ 
     
-    
-    
+       public boolean añadir(Bitacora bitacora){
+        try {
+            sentencias.executeUpdate("insert into bitacoras values(null,'"+bitacora.getPlaca()+"','"+bitacora.getProvincia()+"','"+bitacora.getDestino()+"','"+bitacora.getFechasalida()+"','"+bitacora.getHorasalida()+"','"+bitacora.getKinicial()+"','"+bitacora.getFechallegada()+"','"+bitacora.getHorallegada()+"','"+bitacora.getKfinal()+"')",sentencias.RETURN_GENERATED_KEYS);
+            datos = sentencias.getGeneratedKeys();
+            if (datos.next()) {
+                System.out.println(datos.getInt(1));
+         
+            }   
+            return true;
+        } catch (SQLException ex) {
+            System.out.println("Error al añadir");
+        }
+        return false;
+    }
     
     
     
